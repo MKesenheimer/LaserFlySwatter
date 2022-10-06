0dafe063b205677ef3c4f6e2850e08f26d253817
Bjoern <bjoern_steiner@gmx.de>
2022-10-06 15:05:12 +0200

    Circle detection

--- cv_trackign_test.py
+++ cv_trackign_test.py
@@ -1,10 +1,11 @@
+from types import NoneType
 import cv2 as cv
 import numpy as np 
 import time
 import math
 
 cap = cv.VideoCapture(2)#my webcam(2),default(0)
-_,lf=cap.read()
+
 while(1):
     # Take each frame
     _, frame = cap.read()
@@ -13,27 +14,40 @@ while(1):
     #hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
     
-    change=((lf-frame)%255)
-    cv.imshow('change1',change)
+    
     #hsv_c = cv.cvtColor(change, cv.COLOR_BGR2HSV)
     
     
     # define range of blue color in HSV
-    lower = np.array([245,245,245])
-    upper = np.array([255,255,255])
+    #lower = np.array([245,245,245])
+    #upper = np.array([255,255,255])
 
     # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(change, lower, upper)
+    #mask = cv.inRange(change, lower, upper)
     
     # Bitwise-AND mask and original image
-    res = cv.bitwise_and(change,change, mask= mask)
+    #res = cv.bitwise_and(change,change, mask= mask)
 
 
-    cv.imshow('res',res)
-    cv.imshow('mask',mask)
+    #cv.imshow('res',res)
+    #cv.imshow('mask',mask)
+    parameter1=100
+    img = cv.medianBlur(frame,5)
+    cimg = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    circles = cv.HoughCircles(cimg,cv.HOUGH_GRADIENT,2,30,param1=parameter1,param2=175,minRadius=5,maxRadius=200)
     
+    if type(circles) != NoneType:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            # draw the outer circle
+            cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+
+        cv.imshow('cimg',cimg)
+    cv.imshow("edges", cv.Canny(img,parameter1/2,parameter1))
     cv.imshow('frame',frame)
-    lf=frame
 
     k = cv.waitKey(5) & 0xFF
     if k == 27:
