7629ac9183cc103d4c4ae58e963572298bdc6ffc
Bjoern <bjoern_steiner@gmx.de>
2022-10-06 17:39:48 +0200

    06_01_22

--- cv_trackign_test.py
+++ cv_trackign_test.py
@@ -16,14 +16,18 @@ def update2(value):
     global parameter2
     parameter2=value
 
-
+parameter3=100
+def update3(value):
+    global parameter3
+    parameter3=value
 
 
 
 
 cv.namedWindow("frame")
 cv.createTrackbar('Param1', "frame", 0, 500, update1)
-cv.createTrackbar('Param2', "frame", 0, 500, update2)
+cv.createTrackbar('Threshhold(circles)', "frame", 0, 500, update2)
+cv.createTrackbar('ValueMaskLower', "frame", 0, 255, update3)
 
 while(1):
     # Take each frame
@@ -34,34 +38,34 @@ while(1):
 
     
     
-    #hsv_c = cv.cvtColor(change, cv.COLOR_BGR2HSV)
+    #hsv_c = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     
     
     # define range of blue color in HSV
-    #lower = np.array([245,245,245])
-    #upper = np.array([255,255,255])
+    lower = np.array([parameter3,parameter3,parameter3])
+    upper = np.array([255,255,255])
 
     # Threshold the HSV image to get only blue colors
-    #mask = cv.inRange(change, lower, upper)
+    mask = cv.inRange(frame, lower, upper)
     
     # Bitwise-AND mask and original image
-    #res = cv.bitwise_and(change,change, mask= mask)
-
+    res = cv.bitwise_and(frame,frame, mask= mask)
+    cv.imshow("res",res)
 
     #cv.imshow('res',res)
     #cv.imshow('mask',mask)
     
-    img = cv.medianBlur(frame,5)
+    img = cv.medianBlur(res,3)
     cimg = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
     circles = cv.HoughCircles(cimg,cv.HOUGH_GRADIENT,2,10,param1=parameter1,param2=parameter2,minRadius=5,maxRadius=200)
     
-    if type(circles) != NoneType:
+    if type(circles) != type(None):
         circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            # draw the outer circle
-            cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-            # draw the center of the circle
-            cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+        i=circles[0,0]
+        # draw the outer circle
+        cv.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
 
         cv.imshow('cimg',cimg)
