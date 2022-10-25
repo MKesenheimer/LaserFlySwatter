c0844e1bd664155177db30a69213292331f6ab7a
PaolaGalic <paolagalic1722@gmail.com>
2022-10-25 18:09:58 +0200

    Auskommentiert

--- cv_example1.py
+++ cv_example1.py
@@ -23,8 +23,8 @@ while(1):
     res = cv.bitwise_and(frame,frame, mask= mask)
     
     cv.imshow('frame',frame)
-    cv.imshow('mask',mask)
-    cv.imshow('res',res)
+    #cv.imshow('mask',mask)
+    #cv.imshow('res',res)
 
     k = cv.waitKey(5) & 0xFF
     if k == 27:
